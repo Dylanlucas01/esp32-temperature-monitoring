149554a9ca2ee66e63f98194a94d8feb7f353dce
Dylan Lucas <dylan.v.lucas@gmail.com>
2026-06-23 16:44:47 -0700

    added new db tables locations and temperature_readings

--- models.py
+++ models.py
@@ -3,8 +3,17 @@ from datetime import datetime, timezone
 from extensions import db
 
 
+class Location(db.Model):
+    __tablename__ = "locations"
+
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(80), unique=True, nullable=False)
+    location = db.Column(db.String(120), nullable=False)
+    is_active = db.Column(db.Boolean, default=False, nullable=False)
+
+
 class Reading(db.Model):
-    __tablename__ = "readings"
+    __tablename__ = "temperature_readings"
 
     id = db.Column(db.Integer, primary_key=True)
     recorded_at = db.Column(
@@ -13,10 +22,11 @@ class Reading(db.Model):
         nullable=False,
     )
 
-    location = db.Column(db.String(120), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey("locations.id"), nullable=False)
+    location = db.relationship("Location", backref="readings")
 
-    outside_temp_f = db.Column(db.Float)
+    outside_temperature = db.Column(db.Float)
     outside_humidity = db.Column(db.Float)
 
-    inside_temp_f = db.Column(db.Float)
+    inside_temperature = db.Column(db.Float)
     inside_humidity = db.Column(db.Float)
